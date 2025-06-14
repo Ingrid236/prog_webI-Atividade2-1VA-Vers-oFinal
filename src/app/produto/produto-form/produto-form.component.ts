2ffe9f28ac1172b7e4624fd8f010aaad1abef617
tuorjp <jplmarotinho22@gmail.com>
2025-06-13 22:44:26 -0300

    integração com a API

--- src/app/produto/produto-form/produto-form.component.ts
+++ src/app/produto/produto-form/produto-form.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ProductCreateDto} from '../../api/models/product-create-dto';
+import {ProductControllerService} from '../../api/services/product-controller.service';
+import {Observable} from 'rxjs';
+import {Product} from '../../api/models/product';
 
 @Component({
   selector: 'app-produto-form',
@@ -10,18 +14,37 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class ProdutoFormComponent {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductControllerService
+  ) {
     this.form = this.fb.group({
       name: ['', Validators.required],
+      price: [null, [Validators.required, Validators.min(0.01)]], // Adicionado!
       active: [true]
     });
   }
 
+  createProduct(productData: ProductCreateDto): Observable<Product> {
+    return this.productService.create({ body: productData });
+  }
+
   onSubmit(): void {
     if (this.form.valid) {
-      console.log('Produto cadastrado:', this.form.value);
-      // Futuramente: chamar o serviço para enviar ao backend
+      const newProduct: ProductCreateDto = this.form.value
+      this.createProduct(newProduct).subscribe({
+        next: () => {
+          console.log("Produto criado: " + newProduct.name)
+          this.form.reset({ active: true }); // Reseta o formulário mantendo o toggle ativo
+        },
+        error: (err) => {
+          // É uma boa prática logar o objeto de erro para mais detalhes
+          console.error("Erro ao criar produto:", err);
+        }
+      })
+    } else {
+      // Força a exibição das mensagens de erro se o formulário for enviado inválido
+      this.form.markAllAsTouched();
     }
   }
 }
-

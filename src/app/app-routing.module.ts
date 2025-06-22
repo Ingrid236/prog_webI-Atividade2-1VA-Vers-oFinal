5792f698424a19cab9faf27ce780dde2850a174c
tuorjp <jplmarotinho22@gmail.com>
2025-06-22 18:20:46 -0300

    rotas e importes atualizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponentComponent} from './produto/home-component/home-component.component';
 import {ProdutoFormComponent} from './produto/produto-form/produto-form.component';
 
@@ -7,10 +7,12 @@ const routes: Routes = [
   {path: '', component: HomeComponentComponent},
   {path: 'produtos', component: HomeComponentComponent},
   {path: 'produtos/novo', component: ProdutoFormComponent},
+  {path: 'produtos/editar/:id', component: ProdutoFormComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatButton} from '@angular/material/button';
-import {MatDivider} from '@angular/material/divider';
-import {NgOptimizedImage} from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { AppComponent } from './app.component';
 import { HomeComponentComponent } from './produto/home-component/home-component.component';
 import { ProdutoFormComponent } from './produto/produto-form/produto-form.component';
-import {
-  MatCard,
-  MatCardActions,
-  MatCardContent,
-  MatCardHeader,
-  MatCardSubtitle,
-  MatCardTitle
-} from '@angular/material/card';
-import {MatIcon} from '@angular/material/icon';
-import {MatSlideToggle} from '@angular/material/slide-toggle';
-import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
-import {ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { ProdutoListComponent } from './produto/produto-list/produto-list.component';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponentComponent,
+    ProdutoListComponent,
     ProdutoFormComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MatToolbar,
-    MatButton,
-    MatDivider,
-    NgOptimizedImage,
-    MatCard,
-    MatCardHeader,
-    MatCardContent,
-    MatCardActions,
-    MatIcon,
-    MatCardTitle,
-    MatCardSubtitle,
-    MatFormField,
-    MatLabel,
-    MatSlideToggle,
+    HttpClientModule,
     ReactiveFormsModule,
-    MatInput,
-    MatError,
-    HttpClientModule
+    NgOptimizedImage,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatCardModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTableModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
